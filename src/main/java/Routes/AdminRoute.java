a9f0d6c72dedd1d33b181906b5ed375a6717103a
achenyangqing@163.com <achenyangqing@163.com>
2017-10-13 18:02:28 +0800

    新增interceptor

--- src/main/java/Routes/AdminRoute.java
+++ src/main/java/Routes/AdminRoute.java
@@ -3,11 +3,13 @@ package Routes;
 import com.jfinal.config.Routes;
 
 import controller.AdminController;
+import interceptor.AdminInterceptor;
 
 public class AdminRoute extends Routes{
 
 	@Override
 	public void config() {
+		addInterceptor(new AdminInterceptor());
 		setBaseViewPath("/WEB-INF");
 		add("/admin",AdminController.class);
 		
--- src/main/java/Routes/FrontRoute.java
+++ src/main/java/Routes/FrontRoute.java
@@ -3,6 +3,7 @@ package Routes;
 import com.jfinal.config.Routes;
 
 import controller.IndexController;
+import interceptor.FrontInterceptor;
 
 /**"
  * q前端路由匹配值
@@ -13,8 +14,10 @@ public class FrontRoute extends Routes{
 
 	@Override
 	public void config() {
+		addInterceptor(new FrontInterceptor());//路由级别拦截器interceptor
 		setBaseViewPath("/front");
 		add("/",IndexController.class);
+	  
 		
 	}
 
